5756d690ef48c065dcb564bcc0a7849d795e0a90
Matthew Cassell <mcassell411@gmail.com>
2024-02-21 16:02:35 +0000

    mm/util.c: add page count to __vm_enough_memory failure warning
    Commit 44b414c8715c5dcf53288 ("mm/util.c: add warning if
    __vm_enough_memory fails") adds debug information which gives the process
    id and executable name should __vm_enough_memory() fail.  Adding the
    number of pages to the failure message would benefit application
    developers and system administrators in debugging overambitious memory
    requests by providing a point of reference to the amount of memory causing
    __vm_enough_memory() to fail.
    
    1. Set appropriate kernel tunable to reach code path for failure
       message:
    
    	# echo 2 > /proc/sys/vm/overcommit_memory
    
    2. Test program to generate failure - requests 1 gibibyte per iteration:
    
    	#include <stdlib.h>
    	#include <stdio.h>
    
    	int main(int argc, char **argv) {
    		for(;;) {
    			if(malloc(1<<30) == NULL)
    				break;
    
    			printf("allocated 1 GiB\n");
    		}
    
    		return 0;
    	}
    
    3. Output:
    
    	Before:
    
    	__vm_enough_memory: pid: 1218, comm: a.out, not enough
    memory for the allocation
    
    	After:
    
    	__vm_enough_memory: pid: 1141, comm: a.out, pages: 262145, not
    enough memory for the allocation
    
    Link: https://lkml.kernel.org/r/20240221160235.1771-1-mcassell411@gmail.com
    Signed-off-by: Matthew Cassell <mcassell411@gmail.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Yongqiang Liu <liuyongqiang13@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/util.c
+++ mm/util.c
@@ -976,8 +976,8 @@ int __vm_enough_memory(struct mm_struct *mm, long pages, int cap_sys_admin)
 	if (percpu_counter_read_positive(&vm_committed_as) < allowed)
 		return 0;
 error:
-	pr_warn_ratelimited("%s: pid: %d, comm: %s, not enough memory for the allocation\n",
-			    __func__, current->pid, current->comm);
+	pr_warn_ratelimited("%s: pid: %d, comm: %s, pages: %ld, not enough memory for the allocation\n",
+			    __func__, current->pid, current->comm, pages);
 	vm_unacct_memory(pages);
 
 	return -ENOMEM;
