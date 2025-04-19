8e3b465fd75125d952fef7c3faf64c9025b2f4a5
Shakeel Butt <shakeel.butt@linux.dev>
2025-04-19 11:35:45 -0700

    memcg: introduce non-blocking limit setting option
    Setting the max and high limits can trigger synchronous reclaim and/or
    oom-kill if the usage is higher than the given limit.  This behavior is
    fine for newly created cgroups but it can cause issues for the node
    controller while setting limits for existing cgroups.
    
    In our production multi-tenant and overcommitted environment, we are
    seeing priority inversion when the node controller dynamically adjusts the
    limits of running jobs of different priorities.  Based on the system
    situation, the node controller may reduce the limits of lower priority
    jobs and increase the limits of higher priority jobs.  However we are
    seeing node controller getting stuck for long period of time while
    reclaiming from lower priority jobs while setting their limits and also
    spends a lot of its own CPU.
    
    One of the workaround we are trying is to fork a new process which sets
    the limit of the lower priority job along with setting an alarm to get
    itself killed if it get stuck in the reclaim for lower priority job. 
    However we are finding it very unreliable and costly.  Either we need a
    good enough time buffer for the alarm to be delivered after setting limit
    and potentialy spend a lot of CPU in the reclaim or be unreliable in
    setting the limit for much shorter but cheaper (less reclaim) alarms.
    
    Let's introduce new limit setting option which does not trigger reclaim
    and/or oom-kill and let the processes in the target cgroup to trigger
    reclaim and/or throttling and/or oom-kill in their next charge request. 
    This will make the node controller on multi-tenant overcommitted
    environment much more reliable.
    
    Link: https://lkml.kernel.org/r/20250419183545.1982187-1-shakeel.butt@linux.dev
    Signed-off-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Roman Gushchin <roman.gushchin@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4260,6 +4260,9 @@ static ssize_t memory_high_write(struct kernfs_open_file *of,
 
 	page_counter_set_high(&memcg->memory, high);
 
+	if (of->file->f_flags & O_NONBLOCK)
+		goto out;
+
 	for (;;) {
 		unsigned long nr_pages = page_counter_read(&memcg->memory);
 		unsigned long reclaimed;
@@ -4282,7 +4285,7 @@ static ssize_t memory_high_write(struct kernfs_open_file *of,
 		if (!reclaimed && !nr_retries--)
 			break;
 	}
-
+out:
 	memcg_wb_domain_size_changed(memcg);
 	return nbytes;
 }
@@ -4308,6 +4311,9 @@ static ssize_t memory_max_write(struct kernfs_open_file *of,
 		return err;
 
 	xchg(&memcg->memory.max, max);
+
+	if (of->file->f_flags & O_NONBLOCK)
+		goto out;
 
 	for (;;) {
 		unsigned long nr_pages = page_counter_read(&memcg->memory);
@@ -4336,7 +4342,7 @@ static ssize_t memory_max_write(struct kernfs_open_file *of,
 			break;
 		cond_resched();
 	}
-
+out:
 	memcg_wb_domain_size_changed(memcg);
 	return nbytes;
 }
