794c8eb850ce13d13727defa9f79ad3421ca3080
Vitaly Wool <vitaly.wool@konsulko.se>
2025-05-14 11:54:32 +0200

    rust: zpool integration
    Provide the interface to use zpool in Rust kernel code, exporting
    the necessary C allocation functions along the way.
    
    Signed-off-by: Vitaly Wool <vitaly.wool@konsulko.se>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/kernel/alloc.rs
+++ rust/kernel/alloc.rs
@@ -41,6 +41,11 @@ use core::{alloc::Layout, ptr::NonNull};
 pub struct Flags(u32);
 
 impl Flags {
+    /// Create from the raw representation
+    pub fn new(f: u32) -> Self {
+        Self(f)
+    }
+
     /// Get the raw representation of this flag.
     pub(crate) fn as_raw(self) -> u32 {
         self.0
