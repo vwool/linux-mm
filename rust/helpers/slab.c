053d5569bd6f0324675129af303a16735a0fe3be
Vitaly Wool <vitaly.wool@konsulko.se>
2025-05-14 11:54:32 +0200

    rust: zpool integration
    Provide the interface to use zpool in Rust kernel code
    
    Signed-off-by: Vitaly Wool <vitaly.wool@konsulko.se>

--- rust/helpers/slab.c
+++ rust/helpers/slab.c
@@ -8,6 +8,18 @@ rust_helper_krealloc(const void *objp, size_t new_size, gfp_t flags)
 	return krealloc(objp, new_size, flags);
 }
 
+void * __must_check
+rust_helper_kmalloc(size_t size, gfp_t flags)
+{
+	return kmalloc(size, flags);
+}
+
+void
+rust_helper_kfree(void *ptr)
+{
+	kfree(ptr);
+}
+
 void * __must_check __realloc_size(2)
 rust_helper_kvrealloc(const void *p, size_t size, gfp_t flags)
 {
