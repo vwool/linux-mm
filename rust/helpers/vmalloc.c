bae45b637e2688cc149873959b211e7b03885f7d
Vitaly Wool <vitaly.wool@konsulko.se>
2025-06-11 13:19:03 +0200

    rust: export vmalloc_node
    Export vmalloc_node for future use in zblock_rust.
    
    Signed-off-by: Vitaly Wool <vitaly.wool@konsulko.se>

--- rust/helpers/vmalloc.c
+++ rust/helpers/vmalloc.c
@@ -14,6 +14,12 @@ rust_helper_vmalloc(size_t size)
 	return vmalloc(size);
 }
 
+void * __must_check
+rust_helper_vmalloc_node(size_t size, size_t align, gfp_t gfp_mask, int node, const void *caller)
+{
+	return __vmalloc_node(size, align, gfp_mask, node, caller);
+}
+
 void
 rust_helper_vfree(void *ptr)
 {
